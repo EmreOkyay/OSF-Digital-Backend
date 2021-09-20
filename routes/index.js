a4ca31d781924423b55b36ceec642513f93ec88b
EmreOkyay <okyayatilla@hotmail.com>
2021-09-20 05:18:08 +0300

    added routing for parent Id

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,61 @@
-var express = require('express');
-var router = express.Router();
-var https = require('https');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo');
+const express = require('express');
+const router = express.Router();
+const https = require('https');
+const bodyParser = require('body-parser');
 
 const base_url = 'https://osf-digital-backend-academy.herokuapp.com/api/';
 const secretKey = '$2a$08$wurKWjXAIBE8zHmIsC8wPONR5Dk6X/Ov4zdrR6Rr0BQT5kqQtIq5m';
 
-var router = express();
 
 router.use(express.static('public')); // DON'T FORGET THIS, IMPORTANT  FOR STATIC ASSETS
 
-var product = `${base_url}categories?secretKey=${secretKey}`;
+var baseCategories = `${base_url}categories?secretKey=${secretKey}`;
+var subCategories = `${base_url}categories/:id?secretKey=${secretKey}`;
+var parentCategories = `${base_url}categories/parent/:id?secretKey=${secretKey}`;
+
+let Data = ''; // Global Variable
+
+/* PROBLEMS
+
+- Now, categories parent work but the page loads before the getting elements from the API
+Used setTimeout function for it but need to find a better way
+
+- Parent ID page is trying to load images from a wrong path
+
+*/
+
+// Get Categories by Parent ID
+router.get('/categories/parent/:id', function(req, res, next) {
+
+    let id = req.params.id;
+
+    var subCatUrl = `${base_url}categories/parent/${id}?secretKey=${secretKey}`;
+
+    https.get(subCatUrl, res => {
+        let response = console.log('statusCode:', res.statusCode);
+        let body = '';
+        res.on('data', data => {
+            body += data.toString();
+        });
+
+        res.on('end', () => {
+            const categoryData = JSON.parse(body);
+            Data = categoryData;
+        });
+
+    });
+
+    setTimeout(function(){
+        res.render('categories', { categories: Data,
+            nullData: 'categories/category_404.png',
+            parentId: req.params.id });            
+    },1000);
+
+});
 
 
-https.get(product, res => {
+// Get All Categories
+https.get(baseCategories, res => {
     let response = console.log('statusCode:', res.statusCode);
     let body = '';
     res.on('data', data => {
@@ -26,84 +64,21 @@ https.get(product, res => {
 
     let Data = '';
 
-    // How I can access some api elements, looking at the JSON file from Postman
     res.on('end', () => {
-        const productData = JSON.parse(body);
-        Data = productData;
+        const categoryData = JSON.parse(body);
+        Data = categoryData;
     });
 
     router.get('/categories', function(req, res, next) {
-        res.render('index', { product: Data, nullData: 'categories/category_404.png'});
+        res.render('index', { categories: Data, 
+                              nullData: 'categories/category_404.png'});
     });
 
-    router.get('/categories/:id', function(req, res, next) {
-        res.render('product', { product: Data, 
-                                nullData: 'categories/category_404.png', 
-                                productId: req.params.id});
-    });
-
-    // router.get('/categories/parent/:id', function(req, res, next) {
-    //     res.render('product', { product: Data, 
+    // router.get('/categories/:id', function(req, res, next) {
+    //     res.render('subCategories', { categories: Data, 
     //                             nullData: 'categories/category_404.png', 
-    //                             productId: req.params.id});
+    //                             categoriesId: req.params.id});
     // });
 });
-
-
-// Basically, how I can use the OSF API
-https.get(`${base_url}/products/product_search?id=25565189&secretKey=${secretKey}`, res => {
-    let response = console.log('statusCode:', res.statusCode);
-    let body = '';
-    res.on('data', data => {
-        body += data.toString();
-    });
-
-    let Data = '';
-
-    // How I can access some api elements, looking at the JSON file from Postman
-    res.on('end', () => {
-        const wholeData = JSON.parse(body);
-        console.dir(`----------------------------------------------------------
-        This items name is: ${wholeData[0].page_title} 
-        it's description reads: ${wholeData[0].page_description}
-        and it costs: ${wholeData[0].price}${wholeData[0].currency}
-        ----------------------------------------------------------`);
-        Data = wholeData;
-    });
-
-
-    // Created a let variable named Data and modified it in res.on and show it on homepage when GET reques to /
-    router.get('/api', (req, res) => {
-        res.json(`Product Name: ${Data[0].page_title}`);
-    });
-
-    // How we can pass api variable into .pug file and html
-    // app.get('/', (req, res) => {
-    //     res.render('index', { product: `${Data[0].page_title}` });
-    // });
-
-    // 404 after all routes but before (err, req, res, next)
-    router.use((req, res, next) => {
-        const err = new Error('Not Found');
-        err.status = 404;
-        next(err);
-    });
-
-    // Error middleware, when app encounters an error, express jumps to the first middleware that has 4 parameters like this
-    // app.use((err, req, res, next) => {
-    //     res.locals.error = err;
-    //     res.status(err.status);
-    //     res.render('error');
-    // });
-
-});
-// --------------------------------------------------------------------------------------------------------------
-
-
-
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//     res.render('index', { title: 'MAIN CATEGORY NAME', mainCategoryDescription: 'This is the place holder for main category description' });
-// });
 
 module.exports = router;
