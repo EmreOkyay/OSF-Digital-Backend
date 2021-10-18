e45542135966af2a0b20f457b39b883abbcea1d3
EmreOkyay <55605577+EmreOkyay@users.noreply.github.com>
2021-10-18 14:11:37 +0300

    Fixed some text

--- routes/cart.js
+++ routes/cart.js
@@ -5,8 +5,8 @@ var mid = require('../middleware');
 
 router.use(express.static('public'));
 
-const base_url = 'https://osf-digital-backend-academy.herokuapp.com/api/';
-const secretKey = '$2a$08$wurKWjXAIBE8zHmIsC8wPONR5Dk6X/Ov4zdrR6Rr0BQT5kqQtIq5m';
+const base_url = '<baseUrl>';
+const secretKey = '<secretKey>';
 
 // Get the cart
 router.get('/', mid.requiresLogin, function(request, response, next) {
